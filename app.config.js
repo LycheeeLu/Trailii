efb3968184b98cae66ed1b1ff83f6cc36848282e
lujie <jie.lu@tuni.fi>
2025-09-16 13:49:57 +0300

    :Fix: .env api key React Native setup

---
+++ app.config.js
@@ -0,0 +1,44 @@
+export default {
+  expo: {
+    name: "Trailii",
+    slug: "trailii",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon.png",
+    userInterfaceStyle: "light",
+    splash: {
+      image: "./assets/splash.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    assetBundlePatterns: ["**/*"],
+    ios: {
+      supportsTablet: true,
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+      }
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/adaptive-icon.png",
+        backgroundColor: "#FFFFFF"
+      },
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY
+        }
+      }
+    },
+    web: {
+      favicon: "./assets/favicon.png"
+    },
+    plugins: [
+      [
+        "expo-location",
+        {
+          locationAlwaysAndWhenInUsePermission: "Allow Trailii to use your location to find nearby places and optimize your travel routes."
+        }
+      ]
+    ]
+  }
+};
--- src/constants/config.js
+++ src/constants/config.js
@@ -1,8 +1,8 @@
-export const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
+import { GOOGLE_MAPS_API_KEY, FIREBASE_API_KEY } from '@env';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const FIREBASE_CONFIG = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: FIREBASE_API_KEY,
   authDomain: "traili-cae40.firebaseapp.com",
   projectId: "traili-cae40",
   storageBucket: "traili-cae40.firebasestorage.app",
@@ -11,7 +11,7 @@ export const FIREBASE_CONFIG = {
   measurementId: "G-YP9W6GFFXQ"
 };
 
-
+export const MAPS_API_KEY = GOOGLE_MAPS_API_KEY;
 if (!GOOGLE_MAPS_API_KEY) {
   throw new Error('google map api key is not defined in .env file');
 }
