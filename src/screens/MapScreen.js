8f49e751455610a35722f10053e4b6cfdf9827ff
lujie <jie.lu@tuni.fi>
2025-09-17 12:23:11 +0300

    :Ädd:integrate google map -> screen

--- src/screens/MapScreen.js
+++ src/screens/MapScreen.js
@@ -1,13 +1,66 @@
-import React from "react";
-import {View, Text, StyleSheet} from "react-native";
+import React, {useState, useEffect} from "react";
+import {View, Text, StyleSheet, Alert} from "react-native";
 import { COLORS } from "../constants/config";
+import MapView, { Marker} from 'react-native-maps';
+import * as Location from 'expo-location';
 
 
-const MapScreen = () =>{
+const MapScreen = () => {
+    const [location, setLocation] = useState(null);
+    const [region, setRegion] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            let {status} = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                Alert.alert('Permission to access location was denied','Please enable it to use map');
+
+                return;
+            }
+
+            let currentLocation = await Location.getCurrentPositionAsync({});
+            setLocation(currentLocation);
+            setRegion({
+                latitude: currentLocation.coords.latitude,
+                longitude: currentLocation.coords.longitude,
+                latitudeDelta: 0.001,
+                longitudeDelta: 0.001,
+            });
+
+        })();
+    }, []);
+
+    if (!region){
+        return (
+            <View style={styles.container}>
+                <Text>Loading map...</Text>
+            </View>
+        )
+    }
+
+
 
     return(
         <View style={styles.container}>
             <Text>Map Screen</Text>
+            <MapView
+                style = {styles.map}
+                region = { region}
+                showUserLocation = {true}
+                showMyLocationButton = {true}
+            >
+                {location && (
+                    <Marker
+                        coordinate={{
+                            latitude: location.coords.latitude,
+                            longitude: location.coords.longitude,
+                        }}
+                        title="You are here"
+                        pinColor={COLORS.primary}
+                    />
+                )}
+
+            </MapView>
         </View>
     );
 };
@@ -20,6 +73,19 @@ const styles = StyleSheet.create({
         backgroundColor: COLORS.background,
         alignItems: 'center',
     },
+
+    map:{
+        flex: 1,
+    },
+
+    loadingContainer:{
+        flex:1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: COLORS.background,
+    }
 });
+
+
 
 export default MapScreen;
