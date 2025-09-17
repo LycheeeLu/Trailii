38b848bf957e1f76104628d7f420d1ed35808398
lujie <jie.lu@tuni.fi>
2025-09-17 12:27:44 +0300

    Add: tripprovider context to app.js

--- App.js
+++ App.js
@@ -5,35 +5,42 @@ import MapScreen from './src/screens/MapScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import { TripProvider } from './src/contexts/TripContext';
+import { COLORS } from './src/constants/config';
 
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+    <TripProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
 
-            if (route.name === 'Itinerary') {
-              iconName = focused
-                ? 'list'
-                : 'list-outline';
-            } else if (route.name === 'Map') {
-              iconName = focused ? 'map' : 'map-outline';
-            }
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'tomato',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Itinerary" component={ItineraryScreen} />
-        <Tab.Screen name="Map" component={MapScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+              if (route.name === 'Itinerary') {
+                iconName = focused
+                  ? 'list'
+                  : 'list-outline';
+              } else if (route.name === 'Map') {
+                iconName = focused ? 'map' : 'map-outline';
+              }
+              return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: COLORS.primary,
+            tabBarInactiveTintColor: COLORS.gray,
+            headerStyle: { backgroundColor: COLORS.background },
+            headerTitleStyle: { fontWeight: 'bold' },
+            headerTintColor: COLORS.surface,
+          })}
+        >
+          <Tab.Screen name="Itinerary" component={ItineraryScreen} />
+          <Tab.Screen name="Map" component={MapScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+      </TripProvider>
 
   );
 }
