556aa1cc4ccd6f614773fefb601268a71283ed2f
lujie <jie.lu@tuni.fi>
2025-09-25 12:52:13 +0300

    Change local storage into asyncStorage

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -1,12 +1,30 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import {initializeAuth, getReactNativePersistence} from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { getFirestore } from "firebase/firestore";
 import { FIREBASE_CONFIG } from "../constants/config";
 
 // Initialize Firebase
 const app = initializeApp(FIREBASE_CONFIG);
 
+// initialize firebase auth
+let auth;
+try{
+    auth = initializeAuth(app, {
+        persistence: getReactNativePersistence(AsyncStorage)
+    });
+} catch (error) {
+    // if already initialized then retrieve the previous initialized instance
+    if (error.code === 'auth/already-initialized') {
+    auth = getAuth(app);
+  } else {
+    throw error;
+  }
+
+}
+export {auth} ;
+
 // Initialize Firebase services
-export const auth = getAuth(app);
+
 export const db = getFirestore(app);
 export default app;
